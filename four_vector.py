42c8a4b40ebb027e7597eaf6d36c375542bf0797
mdg137 <michael.d.giles@gmail.com>
2024-04-15 14:16:20 -0400

    Second commit, changes to tests to use fixtures instead of global variables.

--- four_vector.py
+++ four_vector.py
@@ -1,10 +1,10 @@
-from sympy import *
+import sympy as sp
 class fourvector():
     def __init__(self,data,ref_frame):
-        if isinstance(data,Matrix):
+        if isinstance(data,sp.Matrix):
             self.components=data
         else:
-            self.components=Matrix(data)
+            self.components=sp.Matrix(data)
         self.rframe=ref_frame
     def __add__(self,other): #override addition, self-explanatory...
         return fourvector(self.components+other.components,self.rframe)
@@ -19,32 +19,36 @@ class fourvector():
     def __neg__(self):
         return fourvector(-self.components,self.rframe)
     def dot(self,other): #dot product, employing the Lorentz Metric
-        return simplify(self.components.T*Four_reference_frame.lorentz_metric*other.components)
+        return sp.simplify(self.components.T*Four_reference_frame.lorentz_metric*other.components)
     def express(self,other_ref_frame):
         #return a new fourvector that expresses self in another reference frame. In other words, transform a vector as measured in one frame, to as measured in another
         transformation=self.rframe.get_transformation(other_ref_frame)
-        out_vector_components=simplify(transformation*self.components)
+        out_vector_components=sp.simplify(transformation*self.components)
         return fourvector(out_vector_components,other_ref_frame)
 
 class Four_reference_frame:
-    lorentz_metric=Matrix([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,-1]]) 
+    lorentz_metric=sp.Matrix([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,-1]]) 
     rot_matrices={
-        'x':lambda a:Matrix([[1,0,0,0],[0,1,0,0],[0,0,cos(a),-sin(a)],[0,0,sin(a),cos(a)]]),
-        'y':lambda a:Matrix([[1,0,0,0],[0,cos(a),0,sin(a)],[0,0,1,0],[0,-sin(a),0,cos(a)]]), 
-        'z':lambda a:Matrix([[1,0,0,0],[0,cos(a),-sin(a),0],[0,sin(a),cos(a),0],[0,0,0,1]])
+        'x':lambda a:sp.Matrix([[1,0,0,0],[0,1,0,0],[0,0,sp.cos(a),-sp.sin(a)],[0,0,sp.sin(a),sp.cos(a)]]),
+        'y':lambda a:sp.Matrix([[1,0,0,0],[0,sp.cos(a),0,sp.sin(a)],[0,0,1,0],[0,-sp.sin(a),0,sp.cos(a)]]), 
+        'z':lambda a:sp.Matrix([[1,0,0,0],[0,sp.cos(a),-sp.sin(a),0],[0,sp.sin(a),sp.cos(a),0],[0,0,0,1]])
     }
     lor_matrices={
-        'x':lambda b:Matrix([[sqrt(1/(1-b**2)),-b*sqrt(1/(1-b**2)),0,0],[-b*sqrt(1/(1-b**2)),sqrt(1/(1-b**2)),0,0],[0,0,1,0],[0,0,0,1]]),
-        'y':lambda b:Matrix([[sqrt(1/(1-b**2)),0,-b*sqrt(1/(1-b**2)),0],[0,1,0,0],[-b*sqrt(1/(1-b**2)),0,sqrt(1/(1-b**2)),0],[0,0,0,1]]),
-        'z':lambda b:Matrix([[sqrt(1/(1-b**2)),0,0,-b*sqrt(1/(1-b**2))],[0,1,0,0],[0,0,1,0],[-b*sqrt(1/(1-b**2)),0,0,sqrt(1/(1-b**2))]])
+        'x':lambda b:sp.Matrix([[sp.sqrt(1/(1-b**2)),-b*sp.sqrt(1/(1-b**2)),0,0],[-b*sp.sqrt(1/(1-b**2)),sp.sqrt(1/(1-b**2)),0,0],[0,0,1,0],[0,0,0,1]]),
+        'y':lambda b:sp.Matrix([[sp.sqrt(1/(1-b**2)),0,-b*sp.sqrt(1/(1-b**2)),0],[0,1,0,0],[-b*sp.sqrt(1/(1-b**2)),0,sp.sqrt(1/(1-b**2)),0],[0,0,0,1]]),
+        'z':lambda b:sp.Matrix([[sp.sqrt(1/(1-b**2)),0,0,-b*sp.sqrt(1/(1-b**2))],[0,1,0,0],[0,0,1,0],[-b*sp.sqrt(1/(1-b**2)),0,0,sp.sqrt(1/(1-b**2))]])
     }
-    def __init__(self,l=Matrix([1,0,0,0]),i=Matrix([0,1,0,0]),j=Matrix([0,0,1,0]),k=Matrix([0,0,0,1])):
+    def __init__(self,l=sp.Matrix([1,0,0,0]),i=sp.Matrix([0,1,0,0]),j=sp.Matrix([0,0,1,0]),k=sp.Matrix([0,0,0,1])):
         self.i=fourvector(i,self)
         self.j=fourvector(j,self)
         self.k=fourvector(k,self)
         self.l=fourvector(l,self)
-        #transformation matrix that transforms components of vectors to reference frame from the previous frame
+        #transformation sp.Matrix that transforms components of vectors to reference frame from the previous frame
         self.transformation_matrix=l.row_join(i).row_join(j).row_join(k)
+        self.x=sp.Symbol('x',real=True)
+        self.y=sp.Symbol('y',real=True)
+        self.z=sp.Symbol('z',real=True)
+        self.t=sp.Symbol('t',real=True)
         
     def general_boost(self,theta,phi,beta):
         #A general Lorentz Boost works by rotating to orient the z-axis in the current frame with the orientation vector, then by boosting in the new z-direction with boost velocity beta=v/c,
@@ -53,12 +57,11 @@ class Four_reference_frame:
 
         rotation_matrix=Four_reference_frame.rot_matrices['y'](-theta)*Four_reference_frame.rot_matrices['z'](-phi) 
         lorentz_matrix=Four_reference_frame.lor_matrices['z'](beta)
-        transformation_matrix=trigsimp((rotation_matrix.T*lorentz_matrix*rotation_matrix).T) # R*L*R.T
+        transformation_matrix=sp.trigsimp((rotation_matrix.T*lorentz_matrix*rotation_matrix).T) # R*L*R.T
         return Four_reference_frame(transformation_matrix*self.l.components,transformation_matrix*self.i.components,transformation_matrix*self.j.components,transformation_matrix*self.k.components)
         #returns a reference frame with the transformed components as basis vectors
     def get_transformation(self,other):
         #gets transformation between two reference frames, composed of 1) inverted self transformation into the standard basis and 2)transformation to other from standard basis
-        return simplify(other.transformation_matrix*(self.transformation_matrix**-1))
+        return sp.simplify(other.transformation_matrix*(self.transformation_matrix**-1))
 
 
-R=Four_reference_frame()
--- test_four_vector.py
+++ test_four_vector.py
@@ -1,103 +1,126 @@
 import pytest
 from four_vector import *
 R=Four_reference_frame()
-x=Symbol('x',real=True)
-y=Symbol('y',real=True)
-z=Symbol('z',real=True)
-t=Symbol('t',real=True)
-b=Symbol('b',real=True)
-b1=Symbol('b1',real=True)
-b2=Symbol('b2',real=True)
+x=sp.Symbol('x',real=True)
+y=sp.Symbol('y',real=True)
+z=sp.Symbol('z',real=True)
+t=sp.Symbol('t',real=True)
+b=sp.Symbol('b',real=True)
+b1=sp.Symbol('b1',real=True)
+b2=sp.Symbol('b2',real=True)
 v=fourvector([t,x,y,z],R)
+@pytest.fixture
+def ref_frame():
+    return Four_reference_frame()
+@pytest.fixture
+def vector(ref_frame):
+    x=sp.Symbol('x',real=True)
+    y=sp.Symbol('y',real=True)
+    z=sp.Symbol('z',real=True)
+    t=sp.Symbol('t',real=True)
+    return fourvector([t,x,y,z],ref_frame)
+
 #@pytest.mark.skip(reason="I know it passes and takes too long for now")
-def test_general_boost_theta():
-    print(R.general_boost(0,0,b).transformation_matrix)
-    print(Four_reference_frame.lor_matrices['z'](b))
+def test_general_boost_theta(ref_frame):
+    R=ref_frame
+    b=sp.Symbol('b',real=True)
     assert R.general_boost(0,0,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)
-    assert R.general_boost(pi/2,0,b).transformation_matrix==Four_reference_frame.lor_matrices['x'](b)
-    assert R.general_boost(pi,0,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](-b)
-    assert R.general_boost(3*pi/2,0,b).transformation_matrix==Four_reference_frame.lor_matrices['x'](-b)
-    assert R.general_boost(2*pi,0,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)
-    assert R.general_boost(-pi/2,0,b).transformation_matrix==Four_reference_frame.lor_matrices['x'](-b)
-    assert R.general_boost(-pi,0,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](-b)
-    assert R.general_boost(-3*pi/2,0,b).transformation_matrix==Four_reference_frame.lor_matrices['x'](b)
-    assert R.general_boost(-2*pi,0,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)
+    assert R.general_boost(sp.pi/2,0,b).transformation_matrix==Four_reference_frame.lor_matrices['x'](b)
+    assert R.general_boost(sp.pi,0,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](-b)
+    assert R.general_boost(3*sp.pi/2,0,b).transformation_matrix==Four_reference_frame.lor_matrices['x'](-b)
+    assert R.general_boost(2*sp.pi,0,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)
+    assert R.general_boost(-sp.pi/2,0,b).transformation_matrix==Four_reference_frame.lor_matrices['x'](-b)
+    assert R.general_boost(-sp.pi,0,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](-b)
+    assert R.general_boost(-3*sp.pi/2,0,b).transformation_matrix==Four_reference_frame.lor_matrices['x'](b)
+    assert R.general_boost(-2*sp.pi,0,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)
 #@pytest.mark.skip(reason="I know it passes and takes too long for now")
-def test_general_boost_phi():
-    phi=Symbol('phi',real=True)
+def test_general_boost_phi(ref_frame):
+    R=ref_frame
+    b=sp.Symbol('b',real=True)
+    phi=sp.Symbol('phi',real=True)
     assert R.general_boost(0,phi,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)
-    assert R.general_boost(pi,phi,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](-b)
-    assert R.general_boost(-pi,phi,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](-b)
-    assert R.general_boost(2*pi,phi,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)    
-    assert R.general_boost(-2*pi,phi,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)  
-    
+    assert R.general_boost(sp.pi,phi,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](-b)
+    assert R.general_boost(-sp.pi,phi,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](-b)
+    assert R.general_boost(2*sp.pi,phi,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)    
+    assert R.general_boost(-2*sp.pi,phi,b).transformation_matrix==Four_reference_frame.lor_matrices['z'](b)  
 #@pytest.mark.skip(reason="I know it passes and takes too long for now")
-def test_both():
-    phi=Symbol('phi',real=True)
-    theta=Symbol('theta',real=True)
-    assert R.general_boost(theta,phi,b).transformation_matrix==R.general_boost(theta+2*pi,phi,b).transformation_matrix
-    assert R.general_boost(theta,phi,b).transformation_matrix==R.general_boost(theta-2*pi,phi,b).transformation_matrix
-    assert R.general_boost(theta,phi,b).transformation_matrix==R.general_boost(theta,phi+2*pi,b).transformation_matrix
-    assert R.general_boost(theta,phi,b).transformation_matrix==R.general_boost(theta,phi-2*pi,b).transformation_matrix
-    assert R.general_boost(theta,phi,-b).transformation_matrix==R.general_boost(theta+pi,phi,b).transformation_matrix
-    assert R.general_boost(theta,phi,-b).transformation_matrix==R.general_boost(theta-pi,phi,b).transformation_matrix
+def test_both(ref_frame):
+    R=ref_frame
+    b=sp.Symbol('b',real=True)
+    theta=sp.Symbol('theta',real=True)
+    phi=sp.Symbol('phi',real=True)
+    assert R.general_boost(theta,phi,b).transformation_matrix==R.general_boost(theta+2*sp.pi,phi,b).transformation_matrix
+    assert R.general_boost(theta,phi,b).transformation_matrix==R.general_boost(theta-2*sp.pi,phi,b).transformation_matrix
+    assert R.general_boost(theta,phi,b).transformation_matrix==R.general_boost(theta,phi+2*sp.pi,b).transformation_matrix
+    assert R.general_boost(theta,phi,b).transformation_matrix==R.general_boost(theta,phi-2*sp.pi,b).transformation_matrix
+    assert R.general_boost(theta,phi,-b).transformation_matrix==R.general_boost(theta+sp.pi,phi,b).transformation_matrix
+    assert R.general_boost(theta,phi,-b).transformation_matrix==R.general_boost(theta-sp.pi,phi,b).transformation_matrix
     assert R.general_boost(theta,phi,b).transformation_matrix==R.general_boost(theta,phi,b).transformation_matrix.T
 
-
-def test_four_vector_transformations_theta():
-    theta=Symbol('theta',real=True)
+def test_four_vector_transformations_theta(ref_frame,vector):
+    R=ref_frame
+    b=sp.Symbol('b',real=True)
+    b1=sp.Symbol('b1',real=True)
+    theta=sp.Symbol('theta',real=True)
+    v=vector
     assert type(v)==fourvector
     assert v.rframe is R 
     L=R.general_boost(0,0,b)
     q=v.express(L)
     assert q.rframe is L 
-    assert q.components==simplify(L.transformation_matrix*v.components)
+    assert q.components==sp.simplify(L.transformation_matrix*v.components)
     W=L.general_boost(theta,0,b1)
     p=v.express(W)
     assert p.rframe is W
-    print(factor(trigsimp(p.components)))
-    print(trigsimp(W.transformation_matrix*v.components))
-    assert p.components==simplify(W.transformation_matrix*v.components)    
+    print(sp.factor(sp.trigsimp(p.components)))
+    print(sp.trigsimp(W.transformation_matrix*v.components))
+    assert p.components==sp.simplify(W.transformation_matrix*v.components)    
     q2=q.express(W)
     assert q2.rframe is W
-    assert q2.components==simplify(simplify(W.transformation_matrix*(L.transformation_matrix**-1))*q.components)
-    assert v.dot(v)==q.dot(q)
-    assert p.dot(p)==v.dot(v)
-    assert q.dot(q)==p.dot(p)
-    assert q2.dot(q2)==q.dot(q)
-def test_four_vector_transformations_phi():
-    phi=Symbol('phi',real=True)
-    assert type(v)==fourvector
-    assert v.rframe is R 
-    L=R.general_boost(0,0,b)
-    q=v.express(L)
-    assert q.rframe is L 
-    assert q.components==simplify(L.transformation_matrix*v.components)
-    W=L.general_boost(pi/2,phi,b1)
-    p=v.express(W)
-    assert p.rframe is W
-    print(factor(trigsimp(p.components)))
-    print(trigsimp(W.transformation_matrix*v.components))
-    assert p.components==simplify(W.transformation_matrix*v.components)    
-    q2=q.express(W)
-    assert q2.rframe is W
-    assert q2.components==simplify(simplify(W.transformation_matrix*(L.transformation_matrix**-1))*q.components)
+    assert q2.components==sp.simplify(sp.simplify(W.transformation_matrix*(L.transformation_matrix**-1))*q.components)
     assert v.dot(v)==q.dot(q)
     assert p.dot(p)==v.dot(v)
     assert q.dot(q)==p.dot(p)
     assert q2.dot(q2)==q.dot(q)
 
-def test_four_vector_operations():
+def test_four_vector_transformations_phi(ref_frame,vector):
+    R=ref_frame
+    v=vector
+    b=sp.Symbol('b',real=True)
+    b1=sp.Symbol('b1',real=True)
+    phi=sp.Symbol('phi',real=True)
+    assert type(v)==fourvector
+    assert v.rframe is R 
+    L=R.general_boost(0,0,b)
+    q=v.express(L)
+    assert q.rframe is L 
+    assert q.components==sp.simplify(L.transformation_matrix*v.components)
+    W=L.general_boost(sp.pi/2,phi,b1)
+    p=v.express(W)
+    assert p.rframe is W
+    assert p.components==sp.simplify(W.transformation_matrix*v.components)    
+    q2=q.express(W)
+    assert q2.rframe is W
+    assert q2.components==sp.simplify(sp.simplify(W.transformation_matrix*(L.transformation_matrix**-1))*q.components)
+    assert v.dot(v)==q.dot(q)
+    assert p.dot(p)==v.dot(v)
+    assert q.dot(q)==p.dot(p)
+    assert q2.dot(q2)==q.dot(q)
+
+def test_four_vector_operations(ref_frame,vector):
+    R=ref_frame
+    v=vector
     assert v+v==v*2
     assert v-v==v*0
-    q=fourvector([t,x,y,z],R)
+    q=vector
     assert v==q
-    assert not(v is q)
-    c=Symbol('c',real=True)
+    c=sp.Symbol('c',real=True)
     assert c*v==v*c
     q=fourvector([0,1,2,3],R)
     assert v+q==q+v
     assert v-q==-(q-v)
+
+    
 
 
 
